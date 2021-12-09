0b18b889d08f5c786acabc965e21d07016749290
YurkaBest <Ivaschenko_u@ukr.net>
2021-12-09 16:31:47 +0200

    Updates

---
+++ static/js/main.694eaddf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(0),r=c.n(n),i=c(3),o=c.n(i),s=(c(8),c(1)),a=function(){return Object(s.jsx)("div",{})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.694eaddf.chunk.js.map
