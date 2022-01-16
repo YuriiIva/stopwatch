bbc7e0976ed105ba518c7993136a080a2028c1c5
YurkaBest <Ivaschenko_u@ukr.net>
2022-01-16 23:33:36 +0200

    remove the excess

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,9 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+
 import { authOperation } from "redux/auth";
 
-import LoaderB from "components/common/Loader/loader";
-
 import Routes from "components/routes/Routes";
-import Income from "components/Income/Income";
-import Costs from "components/Costs/Costs";
-import HeaderCostsIncome from "components/HeaderCostsIncome/HeaderCostsIncome";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -24,18 +18,6 @@ const App = () => {
   return (
     <>
       <Routes />
-
-      <Suspense fallback={<LoaderB />}>
-        <HeaderCostsIncome />
-        <Switch>
-          <Route exact path="/costs">
-            <Costs />
-          </Route>
-          <Route exact path="/income">
-            <Income />
-          </Route>
-        </Switch>
-      </Suspense>
 
       <ToastContainer position="top-right" autoClose={5000} />
     </>
--- src/components/Costs/Costs.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Costs = () => {
-  return <div>Costs</div>;
-};
-
-export default Costs;
